46fe97cbd475a4e18730a4ab84598ec8eb1dca4a
nateshao <shaotongjie@gmail.com>
2020-11-16 15:05:49 +0800

    初始化完成

---
+++ src/main/java/com/nateshao/NateshaoSpringBootSeckillApplication.java
@@ -0,0 +1,13 @@
+package com.nateshao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NateshaoSpringBootSeckillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NateshaoSpringBootSeckillApplication.class, args);
+    }
+
+}
